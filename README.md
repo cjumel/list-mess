53c2007b080059c8c7d5679c49936941809a0022
cjumel <clement.jumel@gmail.com>
2025-02-05 15:40:02 +0100

    feat: first naive implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # list-mess
-A small commad-line utility to list the mess in a directory, written in Rust
+
+`list-mess` is a small commad-line utility to list the mess in a directory, written in Rust.
+
+It is desiged to easily find the "mess" in a large directory containing code (or, more generally,
+any project versionned with [Git](https://git-scm.com/), that is to list:
+
+- the Git repositories, and wether they are in a "clean" state or not, and
+- the files outside a Git repository.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        let arg = &args[1]; // Index 0 corresponds to debuggging stuff
+        display_dir_content(&Path::new(&arg));
+    } else {
+        display_dir_content(&Path::new("./"));
+    }
+}
+
+fn display_dir_content(path: &Path) {
+    if path.is_dir() == false {
+        println!("not a dir: {}", path.display());
+        return;
+    }
+
+    if path.join(".git/").is_dir() == true {
+        println!("git repo: {}", path.display());
+        return;
+    }
+
+    for child_path in fs::read_dir(&path).unwrap() {
+        let child_path = child_path.unwrap().path();
+        if child_path.is_dir() == true {
+            display_dir_content(&child_path);
+        } else {
+            println!("file: {}", child_path.display())
+        }
+    }
 }
